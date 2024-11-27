faea8568167ff3907c245abb1fc972750cb59c83
이찬희 <cksgml019@naver.com>
2024-11-27 16:52:07 +0900

    Cors 설정 수정

--- src/main/java/hello/autoeverserver/WebConfig.java
+++ src/main/java/hello/autoeverserver/WebConfig.java
@@ -9,11 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 모든 경로에 대해 모든 출처에서 CORS 허용
         registry.addMapping("/**")
-                .allowedOrigins("*")  // 모든 출처에서 요청을 허용
+                .allowedOrigins("http://localhost:3000")  // 특정 출처만 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")  // 허용할 HTTP 메소드
                 .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 자격 증명 포함을 허용 (쿠키 등)
+                .allowCredentials(true);  // 자격 증명 포함을 허용
     }
 }
