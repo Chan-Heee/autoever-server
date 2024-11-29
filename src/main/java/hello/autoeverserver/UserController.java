fd68800278a6066af4d31d169baec565774c9131
이찬희 <cksgml019@naver.com>
2024-11-29 15:47:45 +0900

    [FEATURE] 도커파일 및 revision 파일 추가

--- src/main/java/hello/autoeverserver/UserController.java
+++ src/main/java/hello/autoeverserver/UserController.java
@@ -1,6 +1,7 @@
 package hello.autoeverserver;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/hello/autoeverserver/WebConfig.java
+++ src/main/java/hello/autoeverserver/WebConfig.java
@@ -6,13 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // 특정 출처만 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")  // 허용할 HTTP 메소드
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 자격 증명 포함을 허용
+                .allowedOrigins("http://autoever-front.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
