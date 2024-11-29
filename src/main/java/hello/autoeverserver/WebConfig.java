5b0029aedd07046103ab2662794be490bd40fc02
이찬희 <cksgml019@naver.com>
2024-11-29 17:30:54 +0900

    [REFACTOR] 허용 origin 추가

--- src/main/java/hello/autoeverserver/WebConfig.java
+++ src/main/java/hello/autoeverserver/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://autoever-front.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins(
+                        "http://autoever-front.s3-website.ap-northeast-2.amazonaws.com",
+                        "https://your-ecs-load-balancer-dns-url"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
